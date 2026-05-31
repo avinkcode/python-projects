f912d4355dc06d59f6382b11219e64d872c3c6f1
avinkcode <avin.alert@gmail.com>
2026-05-31 10:38:19 -0500

    add calculator

---
+++ simplecalculator.py
@@ -0,0 +1,8 @@
+
+number1 = input("please enter your first number ")
+number2 = input("please enter your second number ")
+operator = input("please enter one of these operators: +,-,*,/,^  ") 
+if operator == "+":
+    answer = int(number1) + int(number2)
+    print(answer)
+elif
