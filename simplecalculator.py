3b3b74bdbb3c43071189b72bf0eb9c4332c7e7e6
avinkcode <avin.alert@gmail.com>
2026-05-31 10:44:52 -0500

    simplecalcutor

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,8 +1,16 @@
 
 number1 = input("please enter your first number ")
 number2 = input("please enter your second number ")
-operator = input("please enter one of these operators: +,-,*,/,^  ") 
+operator = input("please enter one of these operators: +,-,*,/,  ") 
 if operator == "+":
     answer = int(number1) + int(number2)
     print(answer)
-elif
+elif operator == "-":
+    answer = int(number1) - int(number2)
+    print(answer)
+elif operator == "*":
+    answer = int(number1) * int(number2)
+    print(answer)
+elif operator == "/":
+    answer = int(number1) / int(number2)
+    print(answer)
