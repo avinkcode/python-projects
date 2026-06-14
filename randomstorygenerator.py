952e594e3e7be00892eb1465ceb0c7a4a73b1a5f
avinkcode <avin.alert@gmail.com>
2026-06-14 10:50:05 -0500

    STORY

---
+++ randomstorygenerator.py
@@ -0,0 +1,10 @@
+
+if __name__ == "__main__":
+    character1 = input("give a name for your first character| ")
+    character2 = input("give a name for your second character| ")
+    activity = input("name a active your two characters are doing| ")
+    location = input("name a location your two characters are located at| ")
+    problem = input("name a problem that your two charters are having| ") 
+    solution = input("name a solution that you characters find| ")
+    storyline = f"one day {character1} was playing with {character2} and they were doing {activity}, and were having lots of fun! they were at {location} and were  {problem} and NOT having fun. But then they found a solution! they would {solution}! The End "
+    print(storyline)
