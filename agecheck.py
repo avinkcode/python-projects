0c737283c1a624193690a0307315d27520018a2d
avinkcode <avin.alert@gmail.com>
2026-07-05 09:42:03 -0500

    itknowsyourage

--- agecheck.py
+++ agecheck.py
@@ -2,8 +2,11 @@ from datetime import date
 date1 = date.today()
 print(date1)
 year = int(input("enter your birthday year in YYYY form "))
-month = int(input("enter your briday mont in MM form "))
+month = int(input("enter your birthday month in MM form "))
 day = int(input("enter your birday day in DD form "))
 date2 = date(year, month, day)
 difference = date1 - date2
-print(difference)
+weeks = difference.days / 7
+months = difference.days / 30
+years = difference.days / 365
+print(f"{difference} is equal to {weeks} weeks, and is also equal to {months} months, and you are {years} years old!")
---
+++ startriangle.py
@@ -0,0 +1,6 @@
+number = 20
+star = "*"
+for j in range(number):
+    for i in range(j):
+        print(star, end="")
+    print()   
